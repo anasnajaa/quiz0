54e8c3c515b4fd3c6ebd48a5162441ca83d31c2c
A.N <spidernet12@gmail.com>
2019-04-16 22:24:32 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
  
 Vue.use(UUID);
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
